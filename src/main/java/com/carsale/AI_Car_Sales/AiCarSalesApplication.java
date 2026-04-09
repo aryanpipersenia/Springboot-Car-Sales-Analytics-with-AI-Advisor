6db27ee8553f2756b798879700953784dd25806a
aryanpipersenia <aryanpipersenia@gmail.com>
2026-04-09 21:45:40 +0530

    project created

---
+++ src/main/java/com/carsale/AI_Car_Sales/AiCarSalesApplication.java
@@ -0,0 +1,13 @@
+package com.carsale.AI_Car_Sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCarSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiCarSalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carsale/AI_Car_Sales/AiCarSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carsale.AI_Car_Sales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCarSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
